78ca7585fc10f7794e31be8baeccd2d8581cf0f7
julianaflima <juli.faccio@gmail.com>
2023-03-20 14:19:52 +0530

    Fix menu scroll bar

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -7,6 +7,22 @@ export default `<div id="list-of-dishes">
 	 	<h3>Lorem Ipsum Dolor Sit</h3>
 	 	<p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</p>
 	</div>
+	<div>
+		<h3>Lorem Ipsum</h3>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+	</div>
+	<div>
+	 	<h3>Lorem Ipsum Dolor Sit</h3>
+	 	<p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</p>
+	</div>
+	<div>
+		<h3>Lorem Ipsum</h3>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+	</div>
+	<div>
+	 	<h3>Lorem Ipsum Dolor Sit</h3>
+	 	<p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</p>
+	</div>
 </div>`;
 
 
